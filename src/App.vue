52be94ab7b80479543d18e1c3d477605b7e3bfa3
王 <w127630@126.com>
2020-06-30 17:40:25 +0800

    基础页面

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-addition></my-addition>
+    <my-subtraction></my-subtraction>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
 export default {
-  name: 'App',
+  name: '',
+  data () {
+    return { a: '' }
+  },
   components: {
-    HelloWorld
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
